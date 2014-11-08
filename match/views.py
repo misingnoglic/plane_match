ccddbec5dd01af35e3e3a5c0875a468d4c68e9a3
arya <aboudaie@brandeis.edu>
2014-11-08 17:39:14 -0500

    Added a login to the register

--- match/views.py
+++ match/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 #from django.contrib.auth.forms import UserCreateForm
 from forms import UserCreateForm
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
 # Create your views here.
 
@@ -8,7 +9,8 @@ def register(request):
     if request.method == 'POST':
         form = UserCreateForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request,user)
             return render(request,'created_file.html')
     else:
         form = UserCreateForm()
