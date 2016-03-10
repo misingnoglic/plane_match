4e92021d45cfe8fc97d0e8fa8c323204ca6da9cb
arya <aboudaie@brandeis.edu>
2016-03-10 18:23:27 +0530

    Stopped being a baby and used environmental variables for secrets

---
+++ match/secrets.py
@@ -0,0 +1,4 @@
+__author__ = 'arya'
+amadeus_key = str(os.environ.get('AMADEUS_KEY', 'fail'))
+sendgrid_user = str(os.environ.get('SENDGRID_USER', 'fail'))
+sendgrid_pass = str(os.environ.get('SENDGRID_PASS', 'fail'))
