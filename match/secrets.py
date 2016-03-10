ea715f4ae2412634546c79c879bdcf8833b008f8
arya <aboudaie@brandeis.edu>
2016-03-10 18:24:59 +0530

    This is what I get for not testing before pushing (added import os)

--- match/secrets.py
+++ match/secrets.py
@@ -1,4 +1,5 @@
 __author__ = 'arya'
+import os
 amadeus_key = str(os.environ.get('AMADEUS_KEY', 'fail'))
 sendgrid_user = str(os.environ.get('SENDGRID_USER', 'fail'))
 sendgrid_pass = str(os.environ.get('SENDGRID_PASS', 'fail'))
